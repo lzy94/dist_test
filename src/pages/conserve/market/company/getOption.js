3a5788421819e9d1a749591184fcfd75eb6c1939
lzynfd <15196260302@163.com>
2021-01-15 15:47:04 +0800

    1

--- src/pages/conserve/market/company/getOption.js
+++
@@ -1,8 +0,0 @@
-export default function(option, data) {
-  option.legend = data.legend;
-  option.color = data.color;
-  // option.xAxis.data = data.xAxisData;
-  option.series = data.series;
-
-  return option;
-}
--- src/utils/dataV.js
+++
@@ -1,6 +0,0 @@
-export function dataAddIndex(list) {
-    return list.map((item, index) => ({
-        ...item,
-        id: index + 1
-    }))
-}
